193fe4c5daff723777423886552ec5f5d305da0f
Adrian Rangel <adrian.rangel@gmail.com>
2017-06-22 19:24:37 -0500

    every 12 articles paginate

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,9 @@
 class PagesController < ApplicationController
   def home
     @categories = Category.all
-    @articles = Article.all.page(params[:page]).per(2)
+    @articles = Article.all.page(params[:page]).per(12)
     if params[:slug]
-      @articles = @categories.find_by(slug: params[:slug]).articles.page(params[:page]).per(2)
+      @articles = @categories.find_by(slug: params[:slug]).articles.page(params[:page]).per(12)
     end
     @categories = Category.all
 
