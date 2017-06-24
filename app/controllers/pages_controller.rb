1b9031b5f1b80389838d14d53e0b6644a83d7a7e
gdavila09 <grrd99@gmail.com>
2017-06-23 19:36:40 -0500

    changed id to slug

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < ApplicationController
     if params[:slug]
       @category = @categories.find_by(slug: params[:slug])
       @articles = @category.articles.page(params[:page]).per(2)
-      @sel = @category.id
+      @id = @category.slug
     end
     @categories = Category.all
 
