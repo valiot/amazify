ec86af12f7ac765d9c4ae6ae26cf1f00112607ec
gdavila09 <grrd99@gmail.com>
2017-06-27 11:06:58 -0500

    changed from newest to latest

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,13 @@
 class PagesController < ApplicationController
   def home
     @categories = Category.all
-    @articles = Article.all.page(params[:page]).per(12)
+    @articles = Article.all.page(params[:page]).per(12).order(id: :desc)
     if params[:slug]
       @category = @categories.find_by(slug: params[:slug])
-      @articles = @category.articles.page(params[:page]).per(12)
+      @articles = @category.articles.page(params[:page]).per(12).order(id: :desc)
       @slug = @category.slug
     end
     @categories = Category.all
-
     respond_to do |format|
       format.html
       format.js
