22deefbee85d1a82e36b7f47c2104d32d41de2e5
gdavila09 <grrd99@gmail.com>
2017-06-27 11:06:58 -0500

    changed from newest to latest

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,10 @@
 class PagesController < ApplicationController
   def home
     @categories = Category.all
-    @articles = Article.all.page(params[:page]).per(12)
+    @articles = Article.all.page(params[:page]).per(12).order(:id).reverse_order
     if params[:slug]
       @category = @categories.find_by(slug: params[:slug])
-      @articles = @category.articles.page(params[:page]).per(12)
+      @articles = @category.articles.page(params[:page]).per(12).order(:id).reverse_order
       @slug = @category.slug
     end
     @categories = Category.all
