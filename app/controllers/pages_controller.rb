0464277f62289ea291b136072f50cff8671880a0
gdavila09 <grrd99@gmail.com>
2017-06-27 19:52:59 -0500

    changed the id to updated at for newest to oldest

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,10 @@
 class PagesController < ApplicationController
   def home
     @categories = Category.all
-    @articles = Article.all.page(params[:page]).per(12).order(id: :desc)
+    @articles = Article.all.page(params[:page]).per(12).order(updated_at: :desc)
     if params[:slug]
       @category = @categories.find_by(slug: params[:slug])
-      @articles = @category.articles.page(params[:page]).per(12).order(id: :desc)
+      @articles = @category.articles.page(params[:page]).per(12).order(updated_at: :desc)
       @slug = @category.slug
     end
     @categories = Category.all
