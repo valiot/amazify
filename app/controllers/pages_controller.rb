20c544c7419e789978a78423b246b6184c7cfa84
Adrian Rangel <adrian.rangel@gmail.com>
2017-08-14 13:12:30 -0500

    only show when status is apprvoed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def home
     @categories = Category.all
-    @articles = Article.all.page(params[:page]).per(12).order(created_at: :desc)
+    @articles = Article.where(status: :approved).page(params[:page]).per(12).order(created_at: :desc)
     if params[:slug]
       @category = @categories.find_by(slug: params[:slug])
       @articles = @category.articles.page(params[:page]).per(12).order(created_at: :desc)
