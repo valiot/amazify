b83ac24ad03ac18babf2c0dae232952b518e732f
Juan Dominguez <jdominguez@jcservicio.com>
2017-11-17 23:29:28 -0600

    Hace ID_facebook en caso que cambien de email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
   #has_secure_password
   has_many :articles
   validates_uniqueness_of :email
+  validates_uniqueness_of :id_facebook
   enum role: [:user, :admin]
 end
