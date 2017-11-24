56a82d630983c8d8feccb2988aeb3a0409f95a6f
Juan Dominguez <jdominguez@jcservicio.com>
2017-11-24 11:54:39 -0600

    Espacios, y ruta absoluta

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,15 @@
 class PagesController < ApplicationController
-	def home
-		@categories = Category.all
-		@articles = Article.where(status: :approved).page(params[:page]).per(12).order(created_at: :desc)
-		if params[:slug]
-			@category = @categories.find_by(slug: params[:slug])
-			@articles = @category.articles.page(params[:page]).per(12).order(created_at: :desc)
-			@slug = @category.slug
-		end
-		respond_to do |format|
-			format.html
-			format.js
-		end
-	end
+  def home
+    @categories = Category.all
+    @articles = Article.where(status: :approved).page(params[:page]).per(12).order(created_at: :desc)
+    if params[:slug]
+      @category = @categories.find_by(slug: params[:slug])
+      @articles = @category.articles.page(params[:page]).per(12).order(created_at: :desc)
+      @slug = @category.slug
+    end
+    respond_to do |format|
+      format.html
+      format.js
+    end
+  end
 end
